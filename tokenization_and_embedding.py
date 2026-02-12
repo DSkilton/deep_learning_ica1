527e5405641d8c0329e685d32a13fae9e9b92e75
DSkilton <duncanskilton.ds@gmail.com>
2026-02-12 14:01:05 +0000

    Added mask transformer encoded layer for padded text sequences

--- tokenization_and_embedding.py
+++ tokenization_and_embedding.py
@@ -4,12 +4,13 @@ import tensorflow as tf
 class TokenAndPositionEmbedding(tf.keras.layers.Layer):
     def __init__(self, max_len, vocab_size, embed_dim):
         super().__init__()
+        self.max_len = max_len
         self.token_emb = tf.keras.layers.Embedding(input_dim=vocab_size, output_dim=embed_dim)
         self.pos_emb = tf.keras.layers.Embedding(input_dim=max_len, output_dim=embed_dim)    
         
     def call(self, X):
-        max_len = tf.shape(X)[-1]
-        positions = tf.range(start=0, limit=max_len, delta=1)
+        sequence_length = tf.shape(X)[-1]
+        positions = tf.range(start=0, limit=sequence_length, delta=1)
         positions = self.pos_emb(positions)
         X = self.token_emb(X)
         return X + positions
