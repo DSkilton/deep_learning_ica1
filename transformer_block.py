a91dac606259ab8cd4490a6e461354749755ae84
DSkilton <duncanskilton.ds@gmail.com>
2026-03-04 21:32:57 +0000

    Added params for rnn and amend model to run through the param grid

--- transformer_block.py
+++ transformer_block.py
@@ -5,8 +5,13 @@ class TransformerBlock(tf.keras.layers.Layer):
     def __init__(self, embed_dim, num_heads, feed_forward_dim, rate=0.1, **kwargs):
         super().__init__(**kwargs)
         self.supports_masking = True
+
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.ff_dim = feed_forward_dim
+        self.rate = rate
         
-        self.attention = tf.keras.layers.MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)
+        self.attention = tf.keras.layers.MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim // num_heads, name="multihead_attention")
         self.feed_forward_network = tf.keras.Sequential([
             tf.keras.layers.Dense(feed_forward_dim, activation='relu'),
             tf.keras.layers.Dense(embed_dim)
@@ -18,18 +23,44 @@ class TransformerBlock(tf.keras.layers.Layer):
         self.dropout_att = tf.keras.layers.Dropout(rate)
         self.dropout_ff = tf.keras.layers.Dropout(rate)
 
-    def call(self, inputs, training=False, tensor_mask=None):
+        self.last_attention_scores = None
+
+    def call(self, inputs, training=False, tensor_mask=None, return_attention_scores=False, return_debug=False):
         if tensor_mask is not None:
             tensor_mask = tf.cast(tensor_mask[:, tf.newaxis, tf.newaxis, :], dtype=tf.float32)
 
-        attention_output = self.attention(inputs, inputs, attention_mask=tensor_mask)
+        if return_attention_scores:
+            attention_output, attention_scores = self.attention(
+                inputs, 
+                inputs, 
+                attention_mask=tensor_mask, 
+                return_attention_scores=True,
+            )
+            self.last_attention_scores = attention_scores
+        else: 
+            attention_output = self.attention(
+                inputs, 
+                inputs, 
+                attention_mask=tensor_mask
+            )
+            attention_scores = None
+            self.last_attention_scores = None
+
         attention_output = self.dropout_att(attention_output, training=training)
         output = self.layer_normalization_att(inputs + attention_output)
 
         feed_forward_output = self.feed_forward_network(output)
         feed_forward_output = self.dropout_ff(feed_forward_output, training=training)
 
-        return self.layer_normalization_ff(output + feed_forward_output)
+        X = self.layer_normalization_ff(output + feed_forward_output)
+
+        if return_debug:
+            return X, attention_scores, output, X
+            
+        if return_attention_scores:
+            return X, attention_scores
+            
+        return X
 
     def get_config(self):
         config = super().get_config()
@@ -39,4 +70,4 @@ class TransformerBlock(tf.keras.layers.Layer):
             "ff_dim": self.ff_dim,
             "rate": self.rate,
         })
-    return config
+        return config
